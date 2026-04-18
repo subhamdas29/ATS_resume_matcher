d0d3aed7eddad55a2ad9a3cceb68c83ed5a47317
subhamdas29 <subhamdas5477@gmail.com>
2026-04-19 01:13:29 +0530

    resume and job_desc match score

--- code/main.py
+++ code/main.py
@@ -5,13 +5,31 @@ def main():
     
     #PDF to Text extraction
     extracted_text=get_texts()
-    
+
     #Text to Keywords extraction
-    keywords_list = get_keywords(extracted_text)
-    
-    print("--- Extracted Keywords ---")
-    print(keywords_list)
-    print("---Extracted Text---")
-    print(extracted_text)    
+    resume_keywords = set(get_keywords(extracted_text))
+
+    #Keywords extracted from Job description
+    job_description=set(get_keywords("""
+    We are looking for a Python Junior Developer experienced with FastAPI, 
+    PostgreSQL, and NLTK for natural language processing tasks. 
+    Knowledge of Docker and AWS is a plus.
+    """))
+
+    matched_skills = job_description.intersection(resume_keywords)
+    missing_skills = job_description - resume_keywords
+    match_percentage = (len(matched_skills) / len(job_description)) * 100
+
+    print(f"Match Score: {match_percentage:.2f}%")
+    print(f"Matched: {matched_skills}")
+    print(f"Missing: {missing_skills}")
+
+
+
+
+    # print("--- Extracted Keywords ---")
+    # print(resume_keywords)
+    # print("---Extracted Text---")
+    # print(extracted_text)    
 if __name__ == "__main__":
     main()
